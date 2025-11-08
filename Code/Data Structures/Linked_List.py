d2889d472cb77eca88462ce38d063757be60b19c
Csalais3 <Chrissal0604@gmail.com>
2025-11-08 01:00:36 -0500

    Implemented a doubly linked list

--- Code/Data Structures/Linked_List.py
+++ Code/Data Structures/Linked_List.py
@@ -2,12 +2,25 @@ class Node():
     def __init__(self, data):
         self.data = data
         self._next = None
+        self._prev = None
         
-class LinkedList():
+class SinglyLinkedList():
     def __init__(self):
         self.head = None
         self.tail = None
         self.size = 0
+    
+    def getHead(self) -> Node | None:
+        return self.head
+    
+    def getTail(self) -> Node | None:
+        return self.tail
+    
+    def getSize(self) -> int:
+        return self.size
+    
+    def isEmpty(self) -> bool:
+        return self.size == 0
     
     def add(self, data) -> None:
         if data == None:
@@ -18,24 +31,21 @@ class LinkedList():
             self.size += 1
             return
         
-        curr = self.head
-        while curr._next != None:
-            curr = curr._next
-    
-        curr._next = Node(data)
-        self.tail = curr._next
+        self.tail._next = Node(data)
+        self.tail = self.tail._next
         self.size += 1
             
-        
     def remove(self, data) -> None:
         if data == None:
             raise TypeError("None type is not allowed")
         if self.size == 0:
             raise AttributeError("There is no data")
         if self.size == 1:
-            del self.head
-            self.size -= 1
-            return
+            if self.head.data == data:
+                del self.head
+                self.size -= 1
+                return
+            raise ValueError(f"{data} was not found")
         
         curr = self.head
         while curr._next != None:
@@ -57,7 +67,7 @@ class LinkedList():
             raise TypeError("None type is not allowed")
         if self.size == 0:
             raise AttributeError("There is no data")
-        if index > self.size:
+        if index < 0 or index > self.size:
             raise IndexError("Desired index is larger than the size")
         if index == 0:
             dummy = self.head
@@ -79,7 +89,29 @@ class LinkedList():
         curr._next._next = dummy
         dummy = None
         del dummy
-    
+        
+    def remove_index(self, index: int) -> None:
+        if index < 0 or index > self.size:
+            raise IndexError("Index out of range")
+        if self.size == 0:
+            raise AttributeError("There is no data")
+        
+        curr = self.head
+        if index == 0:
+            if self.size == 1:
+                self.head = None
+            self.head = curr._next
+            del curr
+            self.size -=1
+            return 
+
+        for i in range(index):
+            prev = curr
+            curr = curr._next
+            nexts = curr._next
+        
+        prev._next = nexts
+
     def find(self, data) -> Node:
         if data == None:
             raise TypeError("None type is not allowed")
@@ -97,8 +129,8 @@ class LinkedList():
         raise ValueError(f"{data} was not found")
     
     def index(self, index: int) -> Node:
-        if index < 0:
-            ValueError("Invalid Index")
+        if index < 0 or index > self.size:
+            ValueError("Index out of range")
         if index == 0:
             return self.head
         if index == self.size:
@@ -151,23 +183,276 @@ class LinkedList():
             current = self.reverseh(next, current)
 
         return current
-        
+    
 
     def visualization(self) -> None:
         if self.size == 0:
             return
         
         point = "-->"
-        values = self.all_data()
         visual = ""
         
-        for value in values:
-            visual += (str(value) + " " + point + " ")
+        curr = self.head
+        while curr != None:
+            visual += f" {curr.data} {point}"
+            curr = curr._next
         
         print(visual)    
         
-if __name__ == "__main__":
-    linkedlist = LinkedList()
+class DoublyLinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+        
+    def get_head(self) -> Node | None:
+        return self.head
+    
+    def get_tail(self) -> Node | None:
+        return self.tail
+    
+    def get_size(self) -> int:
+        return self.size
+    
+    def isEmpty(self) -> bool:
+        return self.size == 0
+    
+    def add(self, data: int) -> None:
+        if data == None:
+            raise ValueError("NoneType is not allowed")
+        if self.isEmpty():
+            self.head = Node(data)
+            self.tail = self.head
+            self.size += 1
+            return
+
+        prev = self.tail
+        prev._next = Node(data)
+        self.tail = self.tail._next
+        self.tail._prev = prev
+        self.size += 1
+        
+    def remove(self, data) -> None:
+        if data == None:
+            raise ValueError("NoneType data is not allowed")
+        if  self.isEmpty():
+            raise ValueError("There is no data to remove")
+        if self.size == 1:
+            if data == self.head.data:
+                del self.head
+                self.size -= 1
+                return
+            raise ValueError(f"{data} was not found")
+        
+        if data == self.head.data:
+            curr = self.head
+            self.head = self.head._next
+            del curr
+            self.head._prev = None
+            self.size -= 1
+            return
+        if data == self.tail.data:
+            curr = self.tail
+            self.tail = self.tail._prev
+            del curr
+            self.tail._next = None
+            self.size -= 1
+            return
+        
+        curr1 = self.tail
+        curr2 = self.head
+        while curr1._prev != None and curr2._next != None:
+            curr1 = curr1._prev
+            curr2 = curr2._next
+            if curr1.data == data:
+                prev = curr1._prev
+                nexts = curr1._next
+                prev._next = nexts
+                nexts._prev = prev
+                del curr1
+                self.size -= 1
+                return
+            if curr2.data == data:
+                prev = curr2._prev
+                nexts = curr2._next
+                prev._next = nexts
+                nexts._prev = prev
+                del curr2
+                self.size -= 1
+                return
+
+        raise ValueError(f"{data} was not found")  
+    
+    def add_index(self, index: int, data) -> None:
+        if data == None:
+            raise ValueError("NoneType data is not allowed")
+        if index < 0 or index > self.size:
+            raise IndexError(f"{index} is out of range")
+        if index == 0:
+            head = self.head
+            new = Node(data)
+            new._next = head
+            head._prev = new
+            self.head = new 
+            return
+        if index == self.size:
+            tail = self.tail
+            new = Node(data)
+            new._prev = tail
+            tail._next = new
+            self.tail = new
+            return
+        if self.size == 1:
+            self.add(data)
+            return
+        
+        if index > self.size // 2:
+            curr = self.tail
+            for i in range(self.size - index):
+                curr = curr._prev
+            
+            prev = curr._prev
+            new = Node(data)
+            prev._next = new
+            curr._prev = new
+            new._next = curr
+            new._prev = prev
+            return
+        
+        curr = self.head
+        for i in range (index):
+            curr = curr._next
+        
+        prev = curr._prev
+        new = Node(data)
+        prev._next = new
+        curr._prev = new
+        new._next = curr
+        new._prev = prev
+        return
+    
+    def remove_index(self, index: int) -> None:
+        if index < 0 or index > self.size:
+            raise IndexError(f"{index} is out of range")
+        
+        if index == 0:
+            curr = self.head
+            self.head = curr._next
+            self.head._prev = None
+            del curr
+            return
+        if index == self.size:
+            curr = self.tail
+            self.tail = curr._prev
+            self.tail._next = None
+            del curr
+            return
+
+        if index > self.size // 2:
+            curr = self.tail
+            for i in range(self.size - index):
+                curr = curr._prev
+            
+            prev = curr._prev
+            nexts = curr._next
+            prev._next = nexts
+            nexts._prev = prev
+            del curr
+            return 
+            
+        curr = self.head
+        for i in range(index):
+            curr = curr._next
+        
+        prev = curr._prev
+        nexts = curr._next
+        prev._next = nexts
+        nexts._prev = prev
+        del curr
+        return 
+            
+    def find(self, data) -> Node:
+        if data == None:
+            raise ValueError("NoneType data is not allowed")
+        
+        if self.head.data == data:
+            return self.head
+        if self.tail.data == data:
+            return self.tail
+        
+        curr1 = self.head
+        curr2 = self.tail
+        while curr1._next != None and curr2._prev != None:
+            curr1 = curr1._next
+            curr2 = curr2._prev
+            
+            if curr1.data == data:
+                return curr1
+            if curr2.data == data:
+                return curr2
+        
+        raise ValueError(f"{data} was not found")
+            
+    def index(self, index: int) -> Node:
+        if index < 0 or index > self.size:
+            ValueError("Invalid Index")
+        if index == 0:
+            return self.head
+        if index == self.size:
+            return self.tail
+        
+        if index > self.size // 2:
+            curr = self.tail
+            for i in range(self.size - index):
+                curr = curr._prev
+            
+            return curr
+        
+        curr1 = self.head
+        for i in range(index):
+            curr = curr._next
+        
+        return curr
+    
+    def all_data(self):    
+        if self.isEmpty():
+            return []
+        
+        values = []
+        curr = self.head
+        while curr != None:
+            values.append(curr.data)
+            curr = curr._next
+        
+        return values
+    
+    def reverse(self):
+        curr = self.head
+        
+        for i in range(self.size - 1):
+            prev = curr
+            curr = curr._next
+           
+            prev._next = prev._prev
+            prev._prev = curr
+        
+        self.head, self.tail = self.tail, self.head
+            
+    def visualize(self):
+        out = ""
+        pointer = "<-->"
+        
+        curr = self.head
+        while curr != None:
+            out += f" {curr.data} {pointer}"
+            curr = curr._next
+        
+        print(out)
+        
+def SinglyLinkedListTests():
+    linkedlist = SinglyLinkedList()
+    linkedlist.add(6)
+    linkedlist.remove(6)
     linkedlist.add(1)
     linkedlist.add(2)
     linkedlist.add(5)
@@ -179,7 +464,33 @@ if __name__ == "__main__":
     linkedlist.visualization()
     
     linkedlist.reverse()
+    linkedlist.remove_index(0)
     linkedlist.visualization()
     print(linkedlist.index(0).data)
+    
+    
+def DoublyLinkedListTests():
+    dlinkedlist = DoublyLinkedList()
+    
+    dlinkedlist.add(3)
+    dlinkedlist.add(7)
+    dlinkedlist.add(100)
+    dlinkedlist.add(4.3)
+    dlinkedlist.add("Hello")
+    
+    dlinkedlist.remove(4.3)
+    dlinkedlist.remove_index(0)
+    print(dlinkedlist.find(100))
+    print(dlinkedlist.find(100).data)
+    print(dlinkedlist.all_data())
+    dlinkedlist.visualize()
+    
+    dlinkedlist.reverse()
+    dlinkedlist.visualize()
+
+if __name__ == "__main__":
+    #SinglyLinkedListTests()
+    
+    DoublyLinkedListTests()
 
 
